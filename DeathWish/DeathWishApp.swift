6923c918b809932c050d6e1c314d01403dbf7482
ravidkrisi <ravidkrisi22@gmail.com>
2024-12-11 21:31:21 +0700

    Initial Commit

---
+++ DeathWish/DeathWishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeathWishApp.swift
+//  DeathWish
+//
+//  Created by Ravid Krisi on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeathWishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
