d042285c823857ac35308595aeedaa268ed704c1
ravidkrisi <ravidkrisi22@gmail.com>
2024-12-14 12:54:59 +0700

    [feature] hide back button on HomeView

--- DeathWish/Core/Authentication/HomeView.swift
+++ DeathWish/Core/Authentication/HomeView.swift
@@ -12,15 +12,17 @@ struct HomeView: View {
     
     @Binding var showSignInView: Bool
     var body: some View {
-        Button {
-            Task {
-                try Auth.auth().signOut()
-                showSignInView = true
+        VStack {
+            Button {
+                Task {
+                    try Auth.auth().signOut()
+                    showSignInView = true
+                }
+            } label: {
+                Text("sign out")
             }
-        } label: {
-            Text("sign out")
         }
-
+        .navigationBarBackButtonHidden(true)
     }
 }
 
